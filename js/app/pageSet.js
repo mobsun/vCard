f4eca4e15a1e96fd026d1b4cb732ff1e38240f7c
catest <suijun81@hotmail.com>
2015-05-06 00:12:46 +0800

    html font-size设置

--- js/app/pageSet.js
+++ js/app/pageSet.js
@@ -6,12 +6,12 @@
 		a = 'client';
 		e = document.documentElement || document.body;
 	}
-	//console.log(e[a + 'Width'])
+	
 	var winSize = {
 		width: e[a + 'Width'],
 		height: e[a + 'Height']
 	};
 
-	FontSize = (FontSize * winSize.width) / 640;
-	FontSize = document.getElementsByTagName("html")[0].style.fontSize = FontSize + "px";
+	FontSize = (FontSize * winSize.width) / 640;//取得计算后的html font-size
+	FontSize = document.getElementsByTagName("html")[0].style.fontSize = FontSize + "px";//重新为html设置字号
 })(100)
