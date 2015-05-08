f921a313029c21cc310e3cc86d6a4096eacfee28
catest <suijun81@hotmail.com>
2015-05-08 19:02:07 +0800

    模块加载设置

--- js/config.js
+++ js/config.js
@@ -1,11 +1,4 @@
 require.config({
-	//By default load any module IDs from js/lib
-	//baseUrl: 'js/lib',
-	//except, if the module ID starts with "app",
-	//load it from the js/app directory. paths
-	//config is relative to the baseUrl, and
-	//never includes a ".js" extension since
-	//the paths config could be for a directory.
 	baseUrl:"js",
 	enforceDefine:false,
 	paths: {
