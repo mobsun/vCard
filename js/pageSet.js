5179560dffc597d86211bc535e10909aeb5a2070
catest <suijun81@hotmail.com>
2015-05-08 19:02:55 +0800

    html字号初始化

--- js/pageSet.js
+++ js/pageSet.js
@@ -1,17 +1,15 @@
-(function(Font) {
-	var e = window,
-		a = 'inner',
-		FontSize = Font;
-	if (!('innerWidth' in window)) {
-		a = 'client';
-		e = document.documentElement || document.body;
-	}
-
-	var winSize = {
-		width: e[a + 'Width'],
-		height: e[a + 'Height']
-	};
-
-	FontSize = (FontSize * winSize.width) / 640; //取得计算后的html font-size
-	FontSize = document.getElementsByTagName("html")[0].style.fontSize = FontSize + "px"; //重新为html设置字号
+(function (Font) {
+    var e = window,
+        a = 'inner',
+        FontSize = Font;
+    if (!('innerWidth' in window)) {
+        a = 'client';
+        e = document.documentElement || document.body;
+    }
+    var winSize = {
+        width: e[a + 'Width'],
+        height: e[a + 'Height']
+    };
+    FontSize = (FontSize * winSize.width) / 640; //取得计算后的html font-size
+    FontSize = document.getElementsByTagName("html")[0].style.fontSize = FontSize + "px"; //重新为html设置字号
 })(100)
