8f8c73d4ac8153c80f9dbd855a98e1a5cb3cf437
catest <suijun81@hotmail.com>
2015-05-08 16:29:31 +0800

    删除viewSet.js、windowSize.js (pageSet.js代替以上两个js实现初始化字号功能)

--- js/app/pageSet.js
+++ js/app/pageSet.js
@@ -6,12 +6,12 @@
 		a = 'client';
 		e = document.documentElement || document.body;
 	}
-	
+
 	var winSize = {
 		width: e[a + 'Width'],
 		height: e[a + 'Height']
 	};
 
-	FontSize = (FontSize * winSize.width) / 640;//取得计算后的html font-size
-	FontSize = document.getElementsByTagName("html")[0].style.fontSize = FontSize + "px";//重新为html设置字号
+	FontSize = (FontSize * winSize.width) / 640; //取得计算后的html font-size
+	FontSize = document.getElementsByTagName("html")[0].style.fontSize = FontSize + "px"; //重新为html设置字号
 })(100)
